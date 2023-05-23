fe83f0aeed500c56080528ccd81719a63e24c0b8
Dom <domslee97@gmail.com>
2023-05-23 23:49:35 +1000

    initial commit

--- __test__/index.spec.ts
+++ __test__/index.spec.ts
@@ -1,8 +1,28 @@
 import test from 'ava'
 
-import { plus100 } from '../index'
+import { deleteCredential, readCredential, writeCredential } from '../index'
 
-test('sync function from native code', (t) => {
-  const fixture = 42
-  t.is(plus100(fixture), fixture + 100)
+test('reading and writing credentials', (t) => {
+  writeCredential('wincredentials-node', 'myUsername', 'mySecret')
+  t.deepEqual(readCredential('wincredentials-node'), { username: 'myUsername', secret: 'mySecret' })
+  writeCredential('wincredentials-node', 'myUsername2', 'mySecret2')
+  t.deepEqual(readCredential('wincredentials-node'), { username: 'myUsername2', secret: 'mySecret2' })
+})
+
+test('reading non-existent credential throws exception', (t) => {
+  t.throws(
+    () => {
+      readCredential('wincredentials-node2')
+    },
+    { instanceOf: Error, code: 'InvalidArg', message: 'unable to read_credential: Element not found.' },
+  )
+})
+
+test('deleting non-existent credential throws exception', (t) => {
+  t.throws(
+    () => {
+      deleteCredential('wincredentials-node2')
+    },
+    { instanceOf: Error, code: 'InvalidArg', message: 'unable to delete_credential: Element not found.' },
+  )
 })
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,56 @@
-#![deny(clippy::all)]
-
+use napi::{Error, Status};
 use napi_derive::napi;
+use wincredentials::credential::Credential;
+#[napi]
+/// Writes a credential.
+pub fn write_credential(target: String, username: String, secret: String) -> Result<(), Error> {
+  match wincredentials::write_credential(&target, Credential { username, secret }) {
+    Ok(v) => Ok(v),
+    Err(e) => Err(Error::new(
+      Status::InvalidArg,
+      format!("unable to write_credential: {e}"),
+    )),
+  }
+}
 
 #[napi]
-pub fn plus_100(input: u32) -> u32 {
-  input + 100
+/// Reads a credential. Throws Error with code: 'InvalidArg' if the credential doesn't exist
+pub fn read_credential(target: String) -> Result<NapiCredentials, Error> {
+  let credential = match wincredentials::read_credential(&target) {
+    Ok(v) => v,
+    Err(e) => {
+      return Err(Error::new(
+        Status::InvalidArg,
+        format!("unable to read_credential: {e}"),
+      ))
+    }
+  };
+  Ok(NapiCredentials::from_credential(credential))
+}
+
+#[napi]
+/// Deletes a credential. Throws Error with code: 'InvalidArg' if the credential doesn't exist
+pub fn delete_credential(target: String) -> Result<(), Error> {
+  match wincredentials::delete_credential(&target) {
+    Ok(v) => Ok(v),
+    Err(e) => Err(Error::new(
+      Status::InvalidArg,
+      format!("unable to delete_credential: {e}"),
+    )),
+  }
+}
+
+#[napi(object)]
+pub struct NapiCredentials {
+  pub username: String,
+  pub secret: String,
+}
+
+impl NapiCredentials {
+  pub fn from_credential(credential: Credential) -> Self {
+    Self {
+      username: credential.username,
+      secret: credential.secret,
+    }
+  }
 }
